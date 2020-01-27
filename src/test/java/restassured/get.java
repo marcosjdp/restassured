db8a89c66313742ee654b3eec9e838c01ac1ae42
Marcos Di Paola <marcosjdp@gmail.com>
2020-01-27 20:33:09 -0300

    Reques specification and Response specification

--- src/test/java/restassured/get.java
+++ src/test/java/restassured/get.java
@@ -1,16 +1,21 @@
 package restassured;
 
+import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
 
 
 public class get {
 	
 		 private static RequestSpecification requestSpec;
+		 private static ResponseSpecification responseSpec;
 
 @BeforeClass
 	public static void createRequestSpecification() {
@@ -28,12 +33,31 @@ public class get {
 	        when().
 	            get("us/90210").
 	        then().
+	        spec(responseSpec).
+	        and().
 	            assertThat()
-	            //.body("places.'place name'", hasItem("Beverly Hills"));
-	            .body("country.'$'", hasItem("United  States"));
+	            .body("places.'place name'", hasItem("Beverly Hills"));
+	            //.body("country.'$'", hasItem("United  States"));
 	           // .body("places[0].'place name'", equalTo("Beverly Hills"))
 	            //.body("places[0].'state'", equalTo("California"));
 	    }
+	 @Test
+	    public void prueba2() {
+	    
+	        String placeName =
+	        given().
+	            spec(requestSpec).
+	        when().
+            get("http://zippopotam.us/us/90210").
+	        then().
+	            extract().
+	            path("places[0].'Beverly Hills'");
+
+	        Assert.assertEquals("placeName", "Beverly Hills");
+	        
+	      // System.out.println(placeName);
+	    }
+	
 
 	/* @Test
 	    public void prueba2() {
@@ -45,5 +69,16 @@ public class get {
 	        then()
 	            .log().body();
 	    }*/
+	 
+
+
+	    @BeforeClass
+	    public static void createResponseSpecification() {
+
+	        responseSpec = new ResponseSpecBuilder().
+	            expectStatusCode(200).
+	            expectContentType(ContentType.JSON).
+	            build();
+	    }
 
 }
